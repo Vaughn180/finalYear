70c36fc83bbdb75bdd0a270fe8d5a81519a58e64
nnenne12 <57039882+nnenne12@users.noreply.github.com>
2020-03-10 00:37:55 +0000

    new ht
    h

--- connect.php
+++ connect.php
@@ -13,36 +13,36 @@ $gender = $_POST['gender'];
 $conn = new mysqli('localhost', 'root', 'co600project', 'walkingzebra');
 echo "HELLO";
 
-if(mysqli_connect_errno()){
-    // if there is an error connectiong,  stop the script and dipsay the error message.
-    exit("Failed to connect to MYSQL:" . mysqli_connect_errno());
-}
+// if(mysqli_connect_errno()){
+//     // if there is an error connectiong,  stop the script and dipsay the error message.
+//     exit("Failed to connect to MYSQL:" . mysqli_connect_errno());
+// }
 
-try{
-    $dbhandle = new PDO('mysql:host = localhost, dbname = walkingzebra', 'root','co600project');
-}
-catch(PDOException $e){
-    die('Error Connecting to Database:' .$e->getMessage());
-}
+// try{
+//     $dbhandle = new PDO('mysql:host = localhost, dbname = walkingzebra', 'root','co600project');
+// }
+// catch(PDOException $e){
+//     die('Error Connecting to Database:' .$e->getMessage());
+// }
 
-if ($dbhandle->connect_error){
-    die('connection failed: '.$dbhandle->connection_error);
-} else {
-     $stmt = $dbhandle -> prepare ("INSERT INTO personnel (firstname, lastname, email, password, role, gender) VALUES(?,?,?,?,?,?)")
-     $stmt -> bind_param("ssssss", $fname, $lname, $email, $password, $role, $gender)
-     $stmt -> execute ();
-     echo "reg successful";
-     $stmt -> close();
-     $dbhandle-> close();
-     header('Location: MainPage.html');exit;
-}
+// if ($dbhandle->connect_error){
+//     die('connection failed: '.$dbhandle->connection_error);
+// } else {
+//      $stmt = $dbhandle -> prepare ("INSERT INTO personnel (firstname, lastname, email, password, role, gender) VALUES(?,?,?,?,?,?)");
+//      $stmt = bind_param("ssssss", $fname, $lname, $email, $password, $role, $gender);
+//      $stmt -> execute ();
+//      echo "reg successful";
+//      $stmt -> close();
+//      $dbhandle-> close();
+//      header('Location: MainPage.html');exit;
+// }
 
 //check databse connection
 if ($conn->connect_error){
     die('connection failed: '.$conn->connection_error);
 } else {
-     $stmt = $conn -> prepare ("INSERT INTO personnel (firstname, lastname, email, password, role, gender) VALUES(?,?,?,?,?,?)")
-     $stmt -> bind_param("ssssss", $fname, $lname, $email, $password, $role, $gender)
+     $stmt = $conn -> prepare ("INSERT INTO personnel (firstname, lastname, email, password, role, gender) VALUES(?,?,?,?,?,?)");
+     $stmt -> bind_param("ssssss", $fname, $lname, $email, $password, $role, $gender);
      $stmt -> execute ();
      echo "reg successful";
      $stmt -> close();
